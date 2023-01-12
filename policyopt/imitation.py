f2b14b8bf691d3410bf1a60b103776cc44a70b76
Michael Drolet <mdrolet01@gmail.com>
2023-01-12 09:06:16 +0000

    add dagger

--- policyopt/imitation.py
+++ policyopt/imitation.py
@@ -2,9 +2,96 @@ from . import nn, rl, util, RaggedArray, ContinuousSpace, FiniteSpace, optim, th
 import numpy as np
 from contextlib import contextmanager
 import theano; from theano import tensor
-
+from gail import policyopt
 from scipy.optimize import fmin_l_bfgs_b
+from gail.policyopt import TrajBatch
+import os
+from keras.models import model_from_json
+from pathlib import Path
+import copy
 
+class DAggerOptimizer(object):
+    def __init__(self, mdp, policy, lr, sim_cfg, policy_obsfeat_fn, reward_obsfeat_fn, ex_obs, ex_a, ex_t, enable_obsnorm, policy_hidden_spec):
+        self.mdp, self.policy, self.lr, self.sim_cfg, self.policy_obsfeat_fn, self.reward_obsfeat_fn = mdp, policy, lr, sim_cfg, policy_obsfeat_fn, reward_obsfeat_fn
+        self.x_obs, self.ex_a, self.ex_t = ex_obs, ex_a, ex_t
+        self.enable_obsnorm = enable_obsnorm
+        self.policy_hidden_spec = policy_hidden_spec
+        self.all_trajs = []
+        self.total_num_trajs = 0
+        self.total_num_sa = 0
+        self.total_time = 0.   
+        self.curr_iter = 0
+        self.initial_weights = copy.deepcopy(self.policy.get_params())
+        
+        root_path = Path(os.environ.get('HOME')) / 'irl_control_container/libraries/algorithms/dagger/DAgger'
+        if mdp.env_name == 'MountainCarContinuous-v0':
+            json_file = open(root_path / 'Actor_model_architecture.json', 'r')
+            loaded_model_json = json_file.read()
+            json_file.close()
+            self.mc_policy_fn = model_from_json(loaded_model_json)
+            self.mc_policy_fn.load_weights(root_path / "DDPG_actor_model_750.h5")    
+    
+    def reset_policy(self):
+        self.policy.set_params(self.initial_weights)
+
+    def policy_fn(self, obsfeat_B_Df, env):
+        if self.mdp.env_name.startswith('path_follow'):
+            expert_action = env.dagger_expert_policy_fn()
+            expert_action = np.array(expert_action, dtype=theano.config.floatX).reshape(1,-1)
+        elif self.mdp.env_name == 'MountainCarContinuous-v0':
+            expert_action = self.mc_policy_fn.predict(obsfeat_B_Df)
+            expert_action = np.array(expert_action, dtype=theano.config.floatX).reshape(1,-1)
+        
+        predicted_action, _ = self.policy.sample_actions(obsfeat_B_Df, deterministic=True)
+        return expert_action, predicted_action
+    
+    def step(self):
+        loss_arr = []
+        with util.Timer() as t_all:
+            with util.Timer() as t_sample:
+                sampbatch: TrajBatch = self.mdp.sim_mp(
+                    policy_fn=lambda obsfeat_B_Df, env: self.policy_fn(obsfeat_B_Df, env),
+                    obsfeat_fn=self.policy_obsfeat_fn,
+                    cfg=self.sim_cfg,
+                    alg_name='dagger')
+        
+
+            self.all_trajs += sampbatch.trajs
+            all_traj_batch = TrajBatch.FromTrajs(self.all_trajs)
+            
+            obs_data = all_traj_batch.obs.stacked
+            act_data = all_traj_batch.a.stacked
+            
+            # Do policy updates here
+            self.reset_policy()
+            for i in range(30):
+                loss = self.policy.step_bclone(obs_data, act_data, self.lr)
+                loss_arr.append(loss)
+            
+        self.total_num_trajs += len(sampbatch)
+        self.total_num_sa += sum(len(traj) for traj in sampbatch)
+        self.total_time += t_all.dt
+        
+        fields = [
+            ('iter', self.curr_iter, int),
+            ('trueret', sampbatch.r.padded(fill=0.).sum(axis=1).mean(), float), # average return for this batch of trajectories
+            ('avglen', int(np.mean([len(traj) for traj in sampbatch])), int), # average traj length
+            ('ntrajs', self.total_num_trajs, int), # total number of trajs sampled over the course of training
+            ('nsa', self.total_num_sa, int), # total number of state-action pairs sampled over the course of training
+            # ('avgr', rcurr_stacked.mean(), float), # average regularized reward encountered
+            # ('avgunregr', orig_rcurr_stacked.mean(), float), # average unregularized reward
+            # ('avgpreg', policyentbonus_B.mean(), float), # average policy regularization
+            # ('bcloss', -self.policy.compute_action_logprobs(exbatch_pobsfeat, exbatch_a).mean(), float), # negative log likelihood of expert actions
+            # ('bcloss', np.square(self.policy.compute_actiondist_mean(exbatch_pobsfeat) - exbatch_a).sum(axis=1).mean(axis=0), float),
+            ('tsamp', t_sample.dt, float), # time for sampling
+            # ('tadv', t_adv.dt + t_vf_fit.dt, float), # time for advantage computation
+            # ('tstep', t_step.dt, float), # time for step computation
+            ('ttotal', self.total_time, float), # total time
+            ('loss', loss_arr[-1], float)
+        ]
+
+        self.curr_iter += 1
+        return fields
 
 class BehavioralCloningOptimizer(object):
     def __init__(self, mdp, policy, lr, batch_size, obsfeat_fn, ex_obs, ex_a, eval_sim_cfg, eval_freq, train_frac):
